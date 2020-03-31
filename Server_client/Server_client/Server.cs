9c8b2a17d0088c420adae4f057d24f41c0eeb880
Jakub <kuba.urbanski22@gmail.com>
2020-04-01 00:48:44 +0200

    Created project of server application and simple for now on client. Server can go up now.

---
+++ Server_client/Server_client/Server.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server_client
+{
+    class Server
+    {
+        public static void Main()
+        {
+            TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 80);
+
+            server.Start();
+            Console.WriteLine("Server has started on localhost 127.0.0.1:80. \nWaiting for connection...");
+
+            TcpClient client = server.AcceptTcpClient();
+
+            Console.WriteLine("A client has connected.");
+
+            NetworkStream stream = client.GetStream();
+
+            while (true)
+            {
+                while (!stream.DataAvailable)
+                {
+                    Byte[] bytes = new Byte[client.Available];
+
+                    stream.Read(bytes, 0, bytes.Length);
+                }
+            }
+        }
+    }
+}
