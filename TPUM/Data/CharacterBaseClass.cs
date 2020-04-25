173b6c1d8ff792d9fd63368c9ecf7bf2fe470b1a
Julia Zaborowska <210358@edu.p.lodz.pl>
2020-04-25 14:25:48 +0200

    projekt + warstwy (dane, logika, prezentacja)

---
+++ TPUM/Data/CharacterBaseClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPUM.Data
+{
+    abstract class CharacterBaseClass
+    {
+        public IList<CharacterBaseClass> MultiClasses { get; set; }
+    }
+}
